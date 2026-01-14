351264d428102502cfb8860c77f390f42f365982
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-01-14 23:33:46 +0600

    add custom hook for product types

---
+++ src/hooks/UseProductTypes.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import type IAsideNav from '../types/aside/asideNav';
+import axiosApi from '../api/axiosApi';
+import type IProductTypeApi from '../types/products/productTypeApi';
+
+const UseProductTypes = () => {
+  const [types, setTypes] = useState<IAsideNav[]>([]);
+
+  const getProductTypes = async (): Promise<void> => {
+    const response = await axiosApi.get<IProductTypeApi | null>(
+      'products-type.json'
+    );
+    const productTypesData = response.data;
+
+    if (productTypesData) {
+      const productTypeDataIDS: string[] = Object.keys(productTypesData);
+
+      const rebuiltProductTypes: IAsideNav[] = productTypeDataIDS.map(
+        (id: string) => {
+          return {
+            ...productTypesData[id],
+          };
+        }
+      );
+
+      setTypes(rebuiltProductTypes);
+    }
+  };
+
+  useEffect(() => {
+    void getProductTypes();
+  }, [getProductTypes]);
+
+  return {
+    types,
+  };
+};
+
+export default UseProductTypes;
